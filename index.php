85312d11139e6b09f68560090ec510a69ec161ee
zc1415926 <zc1415926@126.com>
2014-08-05 09:47:10 +0800

    #201408050946 1.完成基础功能构架

---
+++ index.php
@@ -0,0 +1,216 @@
+<form id="params" action="index.php" method="post">
+  	<label>表头有几行？</label><br>
+  	<input type="text" name="rowsOfHead" value="1" /><br>
+  	<label>提取几行？</label><br>
+  	<input type="text" name="rowsOfContent" value="1" /><br>
+  	
+  	<button type="submit">开始合并</button>
+</form>
+<?php
+header ( "Content-Type:text/html; charset=UTF-8" );
+//ini_set('display_errors', false);
+
+// process the xls file
+/**
+ * Include path *
+ */
+set_include_path ( get_include_path () . PATH_SEPARATOR . '/home/zc1415926/www/excel/Classes/' );
+
+/**
+ * PHPExcel_IOFactory
+ */
+include 'PHPExcel/IOFactory.php';
+
+/**
+ * Define a Read Filter class implementing PHPExcel_Reader_IReadFilter
+ */
+class chunkReadFilter implements PHPExcel_Reader_IReadFilter {
+	private $_startRow = 0;
+	private $_endRow = 0;
+		
+	/**
+	 * We expect a list of the rows that we want to read to be passed into the constructor
+	 */
+	public function __construct($startRow, $chunkSize) {
+		$this->_startRow = $startRow;
+		$this->_endRow = $startRow + $chunkSize;
+	}
+	public function readCell($column, $row, $worksheetName = '') {
+		// Only read the heading row, and the rows that were configured in the constructor
+		if (($row == 1) || ($row >= $this->_startRow && $row < $this->_endRow)) {
+			return true;
+		}
+		return false;
+	}
+}
+
+if(isset($_POST["rowsOfHead"]) && isset($_POST["rowsOfContent"]))
+{
+	$resultArray = array();
+	$numOfRowsToSkip=$_POST["rowsOfHead"];// = 1;
+	$numOfRowsToRead=$_POST["rowsOfContent"];// = 2;
+	
+	combineExcels($numOfRowsToSkip, $numOfRowsToRead);
+}
+
+
+
+
+
+function combineExcels($numOfRowsToSkip, $numOfRowsToRead)
+{
+
+echo "<ul>";
+$dirPath = 'uploads/';
+echo $dirPath;
+
+if (! ($handle = opendir ( $dirPath ))) {
+	die ( "不能打开文件夹！" );
+}
+
+while ( $file = readdir ( $handle ) ) {
+	if ($file != "." && $file != "..") {
+		echo "<li>$file</li>";
+		
+		$inputFileType = 'Excel5';
+		// $inputFileType = 'Excel2007';
+		// $inputFileType = 'Excel2003XML';
+		// $inputFileType = 'OOCalc';
+		// $inputFileType = 'Gnumeric';
+		$inputFileName = './uploads/'.$file; // './sampleData/example2.xls';
+		
+
+		
+		
+		
+		
+		//echo 'Loading file ', pathinfo ( $inputFileName, PATHINFO_BASENAME ), ' using IOFactory with a defined reader type of ', $inputFileType, '<br />';
+		/**
+		 * Create a new Reader of the type defined in $inputFileType *
+		 */
+		$objReader = PHPExcel_IOFactory::createReader ( $inputFileType );
+		
+		echo '<hr />';
+		
+		/**
+		 * Define how many rows we want for each "chunk" *
+		 */
+		$chunkSize = 20;
+		
+		/**
+		 * Loop to read our worksheet in "chunk size" blocks *
+		 */
+		for($startRow = 2; $startRow <= 3; $startRow += $chunkSize) {
+			//echo 'Loading WorkSheet using configurable filter for headings row 1 and for rows ', $startRow, ' to ', ($startRow + $chunkSize - 1), '<br />';
+			/**
+			 * Create a new Instance of our Read Filter, passing in the limits on which rows we want to read *
+			 */
+			$chunkFilter = new chunkReadFilter ( $startRow, $chunkSize );
+			/**
+			 * Tell the Reader that we want to use the new Read Filter that we've just Instantiated *
+			 */
+			$objReader->setReadFilter ( $chunkFilter );
+			/**
+			 * Load only the rows that match our filter from $inputFileName to a PHPExcel Object *
+			 */
+			$objPHPExcel = $objReader->load ( $inputFileName );
+			
+			// Do some processing here
+			
+			$sheetData = $objPHPExcel->getActiveSheet ()->toArray ( null, true, true, true );
+			//var_dump ( $sheetData [1] );
+			//echo '<br /><br />';
+			//print_r ( $sheetData [2] );
+			//echo $numOfRowsToRead;
+			//$sheetData = null;
+			//for($startRow = 2; $startRow <= 3; $startRow += $chunkSize) {
+			for($i = 1 + $numOfRowsToSkip; $i <= $numOfRowsToRead + $numOfRowsToSkip; $i++)
+			{
+				//print_r($i);
+				//print_r($sheetData [$i]);
+				echo "<br>!!!!<br>";
+				print_r($sheetData[$i]);
+				echo "<br>";
+				$resultArray[] = $sheetData [$i];
+			}
+		
+			
+		
+		}
+	}
+}
+//print_r($sheetData);
+closedir ( $handle );
+
+echo "</ul>";
+
+$objPHPExcel->disconnectWorksheets();
+unset($objPHPExcel);
+//print_r($resultArray);
+
+
+//create a $resultPHPExcel
+$resultPHPExcel = new PHPExcel();
+//add a worksheet
+$resultWorksheet = new PHPExcel_Worksheet($resultPHPExcel, "Result");
+$resultPHPExcel->addSheet($resultWorksheet, 0);
+$resultPHPExcel->setActiveSheetIndex(0);
+
+//!!!yo an array begin from 0
+
+//echo "<br>haha<br>";
+//print_r($resultArray);
+//echo "<br>haha<br>";
+//print_r($resultArray[1]);
+//echo "<br>haha<br>";
+
+//set cell by loop
+$numOfRows = count($resultArray);
+echo $numOfRows."<br>";
+$numOfColumns = count($resultArray[0]);
+echo $numOfColumns."<br>";
+print_r($resultArray[0]);
+$timesOfColBy26 = 0;
+
+//first param is column, begin with 0, second is row, begin with 1, 
+for($row = 1; $row <= $numOfRows; $row++)
+{
+	for ($col = 1; $col <= $numOfColumns; $col++)
+	{
+		//echo (int)($col / (26 - 1)) . "<br>";//因为从0开始的, 得数为小数
+		$timesOfColBy26 = (int)($col / (26+0.5));//试出来的+0.5，如果-1就少一位，如果+1就多一位，+0.5刚好
+		//echo $timesOfColBy26 . "<br>";
+		//if($timesOfColBy26 == 0)
+		if(1<=$col && $col<=26)
+		{
+			$resultPHPExcel->getActiveSheet()->setCellValueExplicitByColumnAndRow($col-1, $row, $resultArray[$row-1][chr(65+$col-1)]);
+			echo chr(65+$col-1). "<br>";//上边是从1开始的，下边是从0开始的，所以到了下边就要减1
+		}
+		else //只能应对两位字母表示的列数
+		{
+	
+			$resultPHPExcel->getActiveSheet()->setCellValueExplicitByColumnAndRow($col-1, $row, $resultArray[$row-1][chr(65 + $timesOfColBy26-1).chr($col-1 - 26*$timesOfColBy26 + 65)]);
+			echo chr(65 + $timesOfColBy26-1).chr($col-1 - 26*$timesOfColBy26 + 65) . "<br>";//不减1就从B开始了,弄不清楚要不要减一了，试一试就知道了
+	
+			
+		}
+	}
+}
+
+//echo '<br>AAAAAAAAAAAAAAA<br>';
+//echo ord('A');
+
+
+
+
+
+$resultWriter = PHPExcel_IOFactory::createWriter($resultPHPExcel, 'Excel5');
+//$resultWriter->save('php://output');
+$resultWriter->save('./result/result.xls');
+
+
+
+
+//exit;
+}
+?>
---
+++ uploadify/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>UploadiFive Test</title>
+<script src="js/jquery-1.7.2.min.js" type="text/javascript"></script>
+<script src="jquery.uploadify.min.js" type="text/javascript"></script>
+<link rel="stylesheet" type="text/css" href="uploadify.css">
+<style type="text/css">
+body {
+	font: 13px Arial, Helvetica, Sans-serif;
+}
+</style>
+</head>
+
+<body>
+	<h1>Uploadify Demo</h1>
+	
+	<form>
+		<div id="queue"></div>
+		<input id="file_upload" name="file_upload" type="file" multiple="true">
+	</form>
+ <form action="../index.php">
+		<button id="upload_finish" type="submit" disabled="disabled">开始处理</button>
+	</form>
+ 	
+	<script type="text/javascript">
+	//var submitButton =
+		 //document.getElementById("upload_finish").type="hidden";
+	//submitButton.attribute("disabled", "");
+		<?php $timestamp = time();?>
+		$(function() {
+			$('#file_upload').uploadify({
+				'formData'     : {
+					'timestamp' : '<?php echo $timestamp;?>',
+					'token'     : '<?php echo md5('unique_salt' . $timestamp);?>'
+				},
+				'swf'      : 'uploadify.swf',
+				'uploader' : 'uploadify.php',
+				'fileTypeDesc':'Excel 2003 文件',
+				 //允许上传的文件后缀
+				'fileTypeExts':'*.xls;',
+				'buttonText' : '选择文件',
+				//'buttonImage' : 'browse-btn.png'
+				'onUploadSuccess' : function(file, data, response) {
+					var finish = document.getElementById("upload_finish");
+					finish.innerHTML = "wahaha";
+					finish.removeAttribute("disabled");
+				},
+			});
+		});
+	</script>
+
+</body>
+</html>
